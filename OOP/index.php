a9507d559006e066b12c41a94a74c5e89954d8f7
Anton Stepurko <stepurko.anton@gmail.com>
2021-01-27 15:18:11 +0300

    Корректировка вывода

--- OOP/index.php
+++ OOP/index.php
@@ -6,10 +6,13 @@ $queen = new Queen(1, 1);
 $king = new King(4, 3);
 
 try { $queen->Move(7, 3); }
-catch (Exception $e) { echo 'Ошибка: '.$e->getMessage().'\n'; }
+catch (Exception $e) { echo 'Ошибка: '.$e->getMessage().'</br>'; }
 
 try { $king->Move(2, 2); }
-catch (Exception $e) { echo 'Ошибка: '.$e->getMessage().'\n'; }
+catch (Exception $e) { echo 'Ошибка: '.$e->getMessage().'</br>'; }
+
+echo '</br>';
 
 var_dump($queen->getPosition());
+echo '</br>';
 var_dump($king->getPosition());
